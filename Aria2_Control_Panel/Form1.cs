4e63826a4d5d2b495ebc57104296bb4086e07e3c
WIJTB <104023005@live.asia.edu.tw>
2018-08-10 15:10:18 +0800

    fix bug

--- Aria2_Control_Panel/Form1.cs
+++ Aria2_Control_Panel/Form1.cs
@@ -13,6 +13,7 @@ namespace Aria2_Control_Panel
 {
     public partial class Form1 : Form
     {
+       string app_path = Application.StartupPath;
         Process P = new Process();
         public void kill_process ()
         {
@@ -45,7 +46,7 @@ namespace Aria2_Control_Panel
         public void Start_Proccess()
         {
             string conf = @"--conf-path=aria2.conf";
-            P.StartInfo.FileName = "aria2c.exe";
+            P.StartInfo.FileName = app_path + @"\aria2c.exe";
             P.StartInfo.Arguments = conf;
             P.StartInfo.RedirectStandardOutput = true;
             P.StartInfo.UseShellExecute = false;
@@ -92,6 +93,7 @@ namespace Aria2_Control_Panel
         private void Form1_Load(object sender, EventArgs e)
         {
             Check_Process();
+            
         }
 
         private void Information_Box_TextChanged(object sender, EventArgs e)
