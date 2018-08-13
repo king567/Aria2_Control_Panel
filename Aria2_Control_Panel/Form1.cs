6ba9553fce75296cdfdca3a606ad8610c9c25b90
WIJTB <104023005@live.asia.edu.tw>
2018-08-13 17:47:45 +0800

    目前bug式開啟程式後 會自動開機啟動

--- Aria2_Control_Panel/Form1.cs
+++ Aria2_Control_Panel/Form1.cs
@@ -58,6 +58,17 @@ namespace Aria2_Control_Panel
         public Form1()
         {
             InitializeComponent();
+            RegistryKey rgkRun = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            if (rgkRun == null)
+            {
+                checkBox1.Text = "設置開機啟動：目前為關閉狀態";
+                checkBox1.Checked = false;
+            }
+            else
+            {
+                checkBox1.Text = "設置開機啟動：目前為開啟狀態";
+                checkBox1.Checked = true;
+            }
         }
         public void Insert_Text(string content)
         {
@@ -94,9 +105,10 @@ namespace Aria2_Control_Panel
         private void Form1_Load(object sender, EventArgs e)
         {
             Check_Process();
+            // 檢查是否有開機啟動
 
         }
-
+       
         private void Information_Box_TextChanged(object sender, EventArgs e)
         {
 
@@ -106,43 +118,42 @@ namespace Aria2_Control_Panel
         {
 
         }
-
+        
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             if (checkBox1.Checked == true)
             {
-              // 獲得應用進程路徑
-              string strAssName = Application.StartupPath + @"\" + Application.ProductName + @".exe";
-              // 獲得應用進程名稱
-              string strShortFileName = Application.ProductName;
+                    // 獲得應用進程路徑
+                    string strAssName = Application.StartupPath + @"\" + Application.ProductName + @".exe";
+                    // 獲得應用進程名稱
+                    string strShortFileName = Application.ProductName;
+                    // 打開註冊表基項"HKEY_LOCAL_MACHINE"
+                    RegistryKey rgkRun = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                    if (rgkRun == null)
+                    {   // 若不存在，創建註冊表基項"HKEY_LOCAL_MACHINE"
+                        rgkRun = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
+                        MessageBox.Show("添加開機啟動成功");
+                    }
                 
-              // 打開註冊表基項"HKEY_LOCAL_MACHINE"
-              RegistryKey rgkRun = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
-            if (rgkRun == null)
-            {   // 若不存在，創建註冊表基項"HKEY_LOCAL_MACHINE"
-                rgkRun = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
-                MessageBox.Show("添加開機啟動成功");
-            }
-                
-             // 設置指定的註冊表項的指定名稱/值對。如果指定的項不存在，則創建該項。
-                rgkRun.SetValue(strShortFileName, strAssName);
-                MessageBox.Show("添加開機啟動成功");
-                checkBox1.Text = "設置開機啟動：目前為開啟狀態";
+                     // 設置指定的註冊表項的指定名稱/值對。如果指定的項不存在，則創建該項。
+                    rgkRun.SetValue(strShortFileName, strAssName);
+                    MessageBox.Show("添加開機啟動成功");
+                    checkBox1.Text = "設置開機啟動：目前為開啟狀態";
             }
             else if (checkBox1.Checked == false)
             {
-                string strShortFileName = Application.ProductName;
-                // 打開註冊表基項"HKEY_LOCAL_MACHINE"
-                RegistryKey rgkRun = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
-                if (rgkRun == null)
-                {   // 若不存在，創建註冊表基項"HKEY_LOCAL_MACHINE"
+                    string strShortFileName = Application.ProductName;
+                    // 打開註冊表基項"HKEY_LOCAL_MACHINE"
+                    RegistryKey rgkRun = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                    if (rgkRun == null)
+                    {   // 若不存在，創建註冊表基項"HKEY_LOCAL_MACHINE"
                     rgkRun = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
                     MessageBox.Show("已關閉開機啟動");
-                }
-                // 刪除指定的註冊表項的指定名稱/值對。
-                rgkRun.DeleteValue(strShortFileName, false);
-                MessageBox.Show("已關閉開機啟動");
-                checkBox1.Text = "設置開機啟動：目前為關閉狀態";
+                    }
+                    // 刪除指定的註冊表項的指定名稱/值對。
+                    rgkRun.DeleteValue(strShortFileName, false);
+                    MessageBox.Show("已關閉開機啟動");
+                    checkBox1.Text = "設置開機啟動：目前為關閉狀態";
             }
         }
 
